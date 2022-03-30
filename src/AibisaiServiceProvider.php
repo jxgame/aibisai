343dc693d349f98f2ed716654287d9687c9687bb
jxgameHongKong <jxgame@163.com>
2022-03-30 23:33:25 +0800

    init

---
+++ src/AibisaiServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Jxgame\Aibisai;
+
+use Illuminate\Support\ServiceProvider;
+
+class AibisaiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+         // 单例绑定服务 
+        $this->app->singleton('aibisai', function ($app) { 
+            return new Aibisai($app['session'], $app['config']); 
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+     //   $this->loadViewsFrom(__DIR__ . '/views', 'Aibisai'); // 视图目录指定 
+        $this->publishes([ 
+          //  __DIR__.'/views' => base_path('resources/views/vendor/aibisai'),  // 发布视图目录到resources 下 
+            __DIR__.'/config/aibisai.php' => config_path('aibisai.php'), // 发布配置文件到 laravel 的config 下
+
+        ]);
+    }
+}
---
+++ src/Facades/Aibisai.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jxgame\Aibisai\Facades; 
+use Illuminate\Support\Facades\Facade; 
+class Aibisai extends Facade 
+{ 
+    protected static function getFacadeAccessor() 
+    { 
+        return 'aibisai'; 
+    } 
+}
